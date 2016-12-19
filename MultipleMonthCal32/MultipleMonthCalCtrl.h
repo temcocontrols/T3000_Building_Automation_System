2e8b5bcc92cc774f0865b4f1d6a3d935465e99db
Konstantin S. <havendv@gmail.com>
2016-12-19 11:05:12 +1000

    Added MultipleMonthCal32 project.

---
+++ MultipleMonthCal32/MultipleMonthCalCtrl.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SDKDDKVer.h>
+#include <afxdtctl.h>
+#include <vector>
+
+class CMultipleMonthCalCtrl : public CMonthCalCtrl
+{
+	DECLARE_DYNAMIC(CMultipleMonthCalCtrl)
+
+public:
+	CMultipleMonthCalCtrl();
+	virtual ~CMultipleMonthCalCtrl();
+
+	static void   RegisterControl();
+	static void   UnregisterControl();
+
+	void    SetOriginalColors();
+
+	void    EnableMultiselect(int maxSelectCount = 0);
+	void    DisableMultiselect();
+
+	std::vector<SYSTEMTIME>	
+	        GetSelection() const;
+
+	/* This method set last date as first month in calendar.
+	 * Use SetRange() after for select need range.
+	 */
+	void	SetSelection(const std::vector<SYSTEMTIME> & dates);
+
+protected:
+
+	DECLARE_MESSAGE_MAP()
+public:
+
+};
+
+
---
+++ MultipleMonthCal32/multiplemonthcal.h
@@ -0,0 +1,47 @@
+#ifndef __IMPROVED_MONTHCAL_H
+#define __IMPROVED_MONTHCAL_H
+
+#include <SDKDDKVer.h>
+#include <afx.h>
+#include <afxdtctl.h>
+#include <Windows.h>
+#include <uxtheme.h>
+#include "unicode.h"
+
+#define Alloc(s) malloc((s))
+#define ReAlloc(s,t) realloc((s),(t))
+#define Free(s) free((s))
+#define GetSize(s) GetSize((s));
+
+#define MCSC_SELECTEDTEXT      6  // selection text color
+#define MCSC_SELECTEDBK        7  // selection background color
+#define MCSC_ABBREVIATIONSTEXT 8  // abbreviations text color
+#define MCSC_ABBREVIATIONSBK   9  // abbreviations background color
+#define MCSC_ABBREVIATIONSLINE 10 // abbreviations line color
+
+typedef struct tagSELECTION_ITEM
+{
+	SYSTEMTIME          time;
+	tagSELECTION_ITEM * next;
+} SELECTION_ITEM, *LPSELECTION_ITEM;
+
+typedef struct
+{
+	INT               size;
+	LPSELECTION_ITEM  first;
+} SELECTION_INFO, *LPSELECTION_INFO;
+
+// MCN_SELCHANGEEX is sent whenever the currently displayed date changes
+// via month change, year change, keyboard navigation, prev/next button
+//
+typedef struct tagNMSELCHANGEEX
+{
+	NMHDR           nmhdr;  // this must be first, so we don't break WM_NOTIFY
+
+	SELECTION_INFO  selectionInfo;
+} NMSELCHANGEEX, *LPNMSELCHANGEEX;
+
+VOID   MONTHCAL_Register(VOID);
+VOID   MONTHCAL_Unregister(VOID);
+
+#endif  /* __IMPROVED_MONTHCAL_H */
