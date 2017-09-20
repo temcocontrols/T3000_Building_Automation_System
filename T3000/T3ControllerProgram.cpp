96755f48326f3e7d531515eaa6323743902ddd86
Alex  Zhang <Alex@temcocontrols.com>
2017-09-20 21:20:46 +0800

    Add T3 Program Class to deal with Item4
    Item4: Editor losing expansion I/O points.

---
+++ T3000/T3ControllerProgram.cpp
@@ -0,0 +1,25 @@
+#include "StdAfx.h"
+#include "T3ControllerProgram.h"
+
+#include"global_function.h"
+CT3ControllerProgram::CT3ControllerProgram()
+{
+}
+
+
+CT3ControllerProgram::~CT3ControllerProgram()
+{
+}
+
+
+int CT3ControllerProgram::GetProgramData(uint32_t deviceid, uint8_t start_instance, uint8_t end_instance, uint8_t npackgae)
+{
+
+
+	return GetProgramData(deviceid, start_instance, end_instance, npackgae);
+}
+
+int CT3ControllerProgram::GetProgramData_Blocking(uint32_t deviceid, uint8_t start_instance, uint8_t end_instance, uint8_t npackgae)
+{
+	return GetProgramData_Blocking(deviceid, start_instance, end_instance, npackgae);
+}
