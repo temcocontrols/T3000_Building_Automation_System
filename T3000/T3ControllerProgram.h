0fb41499e503610a9495202f74bcec27b7249015
Alex  Zhang <Alex@temcocontrols.com>
2017-09-26 18:10:22 +0800

    update 2017/9/26

--- T3000/T3ControllerProgram.h
+++ T3000/T3ControllerProgram.h
@@ -10,9 +10,13 @@ public:
 	/*int GetPrivateData(uint32_t deviceid, uint8_t command, uint8_t start_instance, uint8_t end_instance, int16_t entitysize);
 	int GetPrivateData_Blocking(uint32_t deviceid, uint8_t command, uint8_t start_instance, uint8_t end_instance, int16_t entitysize);
 */
-	int WriteProgramData(uint32_t deviceid, uint8_t n_command, uint8_t start_instance, uint8_t end_instance, uint8_t npackage);
+	int SaveProgramData(uint32_t deviceid, uint8_t n_command, uint8_t start_instance, uint8_t end_instance, uint8_t npackage);
 	 
-	bool Check_FullLabel_Exsit(LPCTSTR m_new_fulllabel);
-	bool Check_Label_Exsit(LPCTSTR m_new_fulllabel);
+	bool Judge_FullLabel_Exsit(LPCTSTR m_new_fulllabel);
+	bool Judge_Label_Exsit(LPCTSTR m_new_fulllabel);
+
+	void SplitStringToArray(CStringArray &saArray, CString sSource, CString sToken);
+	void Inial_Product_map();
+	void Initial_Product();
 };
 
