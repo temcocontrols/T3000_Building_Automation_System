c83665b3b50a0a45f5c1e02f842f02d911891d76
TemcoAlex <Alex@temcocontrols.com>
2017-11-17 20:29:04 +0800

    Create a class to support the log data from T3000

---
+++ Log/ILog.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace Utils
+{
+   
+    public interface ILog
+    {
+       
+        void WriteAction(string text, Log.ActTypes actType);
+
+        void WriteInfo(string text);
+
+        
+        void WriteAction(string text);
+
+     
+        void WriteError(string text);
+
+       
+        void WriteException(Exception ex, string errMsg = "", params object[] args);
+
+     
+        void WriteLine(string text = "");
+
+        void WriteBreak();
+    }
+}
---
+++ Log/LogStub.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace Utils
+{
+  
+    public class LogStub : ILog
+    {
+       
+        public void WriteAction(string text, Log.ActTypes actType)
+        {
+        }
+
+       
+        public void WriteInfo(string text)
+        {
+        }
+
+      
+        public void WriteAction(string text)
+        {
+        }
+
+     
+        public void WriteError(string text)
+        {
+        }
+
+      
+        public void WriteException(Exception ex, string errMsg = "", params object[] args)
+        {
+        }
+
+      
+        public void WriteLine(string text = "")
+        {
+        }
+
+       
+        public void WriteBreak()
+        {
+        }
+    }
+}
