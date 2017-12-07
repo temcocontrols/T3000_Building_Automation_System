436047b2a9cd43ea7bc8a5a1ed2d63607bddc2b2
Alex  Zhang <Alex@temcocontrols.com>
2017-12-07 17:34:57 +0800

    Get Product list from BroveControls.com

---
+++ BroveApp/TemcoProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroveApp
+{
+    class TemcoProduct
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Weight { get; set; }
+        public string ImagesLink { get; set; }
+    }
+}
