8731230beddf4967f73abad8f9f8d8fa77762d28
Alex  Zhang <Alex@temcocontrols.com>
2018-03-01 10:44:48 +0800

    T3000 Update
    1.MonthController
       update the click event
       fixed the issue that it is not easy to select the data.
    2.update programeditor
       supported the color of the keywords ,variables,inputs,outputs.

---
+++ MultipleMonthCal32/MonthControl.cs
@@ -0,0 +1,108 @@
+﻿namespace T3000.Controls.MultipleMonthCalendarControls
+{
+    using System;
+    using System.Linq;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using System.ComponentModel;
+    using System.Collections.Generic;
+
+    internal partial class MonthControl : UserControl
+    {
+        #region Properties
+
+        private DateTime _date = DateTime.Today;
+        [Description("Is selected"), Category("DayControl")]
+        public DateTime Date {
+            get { return _date; }
+            set {
+                _date = value;
+
+                UpdateDays();
+            }
+        }
+
+        public List<DateTime> SelectedDays =>
+            Days.Where(i => i.IsSelected).Select(i => i.Date).ToList();
+
+        public void SelectDay(DateTime date)
+        {
+            var day = Days.Where(i => i.Date.Equals(date)).FirstOrDefault();
+            if (day != null)
+            {
+                day.IsSelected = true;
+            }
+        }
+
+        public void UnselectDay(DateTime date)
+        {
+            var day = Days.Where(i => i.Date.Equals(date)).FirstOrDefault();
+            if (day != null)
+            {
+                day.IsSelected = false;
+            }
+        }
+
+        #endregion
+
+        #region Days
+
+        private List<DayControl> Days = new List<DayControl>();
+        private void InitializeDays()
+        {
+            var width = (Width - 10) / 7.0;
+            var heigth = (Height - 10 - 40) / 5.0;
+            var size = new Size(Convert.ToInt32(width), Convert.ToInt32(heigth));
+            for (var i = 0; i < 35; ++i)
+            {
+                var x = i % 7;
+                var y = i / 7;
+
+                var day = new DayControl();
+                day.Left = 5 + x * size.Width;
+                day.Top = 45 + y * size.Height;
+                day.Size = size;
+                day.Width += 3;
+                day.Click += (sender, args) => day.IsSelected = !day.IsSelected;
+                Controls.Add(day);
+                Days.Add(day);
+            }
+
+            Invalidate();
+        }
+
+        private void UpdateDays()
+        {
+            var offset = (int)Date.DayOfWeek;
+            for (var i = 0; i < 35; ++i)
+            {
+                var day = Days[i];
+                var isCurrentMonth = i - offset >= 0 && 
+                    i - offset < DateTime.DaysInMonth(Date.Year, Date.Month);
+                day.Date = isCurrentMonth
+                    ? new DateTime(Date.Year, Date.Month, i - offset + 1)
+                    : new DateTime();
+            }
+            titleButton.Text = Date.ToString("MMMM yyyy");
+
+            Invalidate();
+        }
+
+        #endregion
+
+        public MonthControl()
+        {
+            InitializeComponent();
+            InitializeDays();
+
+            ResizeRedraw = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.DrawLine(SystemPens.ControlDark, 0, 40, Width, 40);
+        }
+    }
+}
---
+++ MultipleMonthCal32/Utilities.cs
@@ -0,0 +1,69 @@
+﻿namespace T3000.Controls.MultipleMonthCalendarControls
+{
+    using System;
+
+    public static class MonthUtilities
+    {
+        public static int MonthDiff(DateTime time1, DateTime time2) =>
+            (time2.Year - time1.Year) * 12 + time2.Month - time1.Month;
+
+        public static int MonthLength(int year, int month) =>
+            DateTime.DaysInMonth(year, month);
+
+        public static bool IsDateEquals(DateTime time1, DateTime time2) =>
+            time1.Year == time2.Year &&
+            time1.Month == time2.Month &&
+            time1.Day == time2.Day;
+
+        /// <summary>
+        /// Copies timestamp part only
+        /// </summary>
+        public static DateTime CopyTime(DateTime from, DateTime to) =>
+            new DateTime(to.Year, to.Month, to.Day, from.Hour, from.Minute, from.Second);
+
+        /// <summary>
+        /// Copies date part only
+        /// </summary>
+        public static DateTime CopyDate(DateTime from, DateTime to) =>
+            new DateTime(from.Year, from.Month, from.Day, to.Hour, to.Minute, to.Second);
+
+        public static int CompareMonths(DateTime date1, DateTime date2)
+        {
+            var monthDate1 = new DateTime(date1.Year, date1.Month, 1);
+            var monthDate2 = new DateTime(date2.Year, date2.Month, 1);
+
+            return DateTime.Compare(monthDate1, monthDate2);
+        }
+
+        public static int CompareDate(DateTime date1, DateTime date2)
+        {
+            var monthDate1 = new DateTime(date1.Year, date1.Month, date1.Day);
+            var monthDate2 = new DateTime(date2.Year, date2.Month, date2.Day);
+
+            return DateTime.Compare(monthDate1, monthDate2);
+        }
+
+        /// <summary>
+        /// Adds/subtracts 'months' from date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="months"></param>
+        /// <returns></returns>
+        public static DateTime GetMonth(DateTime date, int months)
+        {
+            var m = date.Month + months;
+
+            var newYear = date.Year + (m > 0 ? (m - 1) / 12 : m / 12 - 1);
+            var newMonth = m > 0 ? (m - 1) % 12 + 1 : 12 + m % 12;
+
+            /* fix moving from last day in a month */
+            var length = MonthLength(newYear, newMonth);
+
+            return new DateTime(newYear, newMonth, date.Day > length ? length : date.Day);
+        }
+
+        public static DateTime GetNextMonth(DateTime date) => GetMonth(date, 1);
+        public static DateTime GetPrevMonth(DateTime date) => GetMonth(date, -1);
+
+    }
+}
