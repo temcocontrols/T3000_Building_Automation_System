3e04e0e01381f7fbf5b1eb0f6946e0150c60cad2
Vivek C <c_vivek@rediffmail.com>
2018-05-07 20:55:45 +0530

    Removed the 'at' function access on a BitSet and used direct indexing instead. This now compiles in versions greater than or equal to C++11.

--- T3000/BacnetMonitor.cpp
+++ T3000/BacnetMonitor.cpp
@@ -1607,8 +1607,11 @@ int read_monitordata(int digtal_or_analog,unsigned int timeleft,unsigned int tim
         {
             if (read_index < read_analog_package.size())
             {
-                if ((read_analog_package.at(read_index) == true) && (read_index != m_monitor_head.seg_index + m_monitor_head.total_seg))  //已经读过的;
-                    continue;
+				// See the corresponding comment below as to why direct indexing of bitset is valid here.
+				if (read_analog_package[read_index] && (read_index != m_monitor_head.seg_index + m_monitor_head.total_seg))
+				{
+					continue;
+				}
             }
             else
             {
@@ -1619,8 +1622,13 @@ int read_monitordata(int digtal_or_analog,unsigned int timeleft,unsigned int tim
         {
             if (read_index < read_dig_package.size())
             {
-                if ((read_dig_package.at(read_index) == true) && (read_index != m_monitor_head.seg_index + m_monitor_head.total_seg))  //已经读过的;
-                    continue;
+				// Using 'at' for indexing is not really required here due to the below two facts:
+				// 1. The conditional check above ensures we are well within the bounds
+				// 2. The starting index is anyway 0 and incremented by 1, hence -ve bound check not necessary.
+				if (read_dig_package[read_index] && (read_index != m_monitor_head.seg_index + m_monitor_head.total_seg))
+				{
+					continue;
+				}
             }
             else
             {
