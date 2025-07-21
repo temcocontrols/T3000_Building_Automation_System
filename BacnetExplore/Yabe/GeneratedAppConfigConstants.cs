c84e01d887efe0ef743c1935417fd7a3e021234a
Fance <fandu@temcocontrols.com>
2025-07-21 11:12:47 +0800

    Fix Confict

--- BacnetExplore/Yabe/GeneratedAppConfigConstants.cs
+++ BacnetExplore/Yabe/GeneratedAppConfigConstants.cs
@@ -6,6 +6,6 @@ namespace Yabe
 public static class AppConfigConstants
 {
 public const string AppConfigName = "T3000_config.ini";
-public const string AppBacnetName = "T3000 Standard BacNET Tool";
+public const string AppBacnetName = "T3000 Standard BACNet Tool";
 }
 }
